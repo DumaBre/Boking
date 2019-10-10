748778c9a00411c3267daf6fd20dee7187a415a0
dusan antonijevic <duma-93@hotmail.com>
2019-10-10 15:17:02 +0200

    Added events,some func

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import Modal from "../components/Modal/Modal";
 import Backdrop from "../components/Backdrop/Backdrop";
+import AuthContext from "../context/auth-context";
 
 import "./Events.css";
 
 export default class Events extends Component {
   state = {
-    creating: false
+    creating: false,
+    events: []
   };
+
+  static contextType = AuthContext;
+
+  constructor(props) {
+    super(props);
+    this.titleElRef = React.createRef();
+    this.priceElRef = React.createRef();
+    this.dateElRef = React.createRef();
+    this.descriptionElRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.fatchEvents();
+  }
 
   startCreatEventHandler = () => {
     this.setState({ creating: true });
@@ -15,13 +31,118 @@ export default class Events extends Component {
 
   modalConfirmHandler = () => {
     this.setState({ creating: false });
+    const title = this.titleElRef.current.value;
+    const price = +this.priceElRef.current.value;
+    const date = this.dateElRef.current.value;
+    const description = this.descriptionElRef.current.value;
+    if (
+      title.trim().lenght === 0 ||
+      price <= 0 ||
+      date.trim().lenght === 0 ||
+      description.trim().lenght === 0
+    ) {
+      return;
+    }
+
+    const event = { title, price, date, description };
+    console.log(event);
+
+    const requestBody = {
+      query: `
+        mutation {
+          createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price}, date: "${date}"}){
+            _id
+            title
+            description
+            price
+            date
+            creator{
+              _id
+              email
+            }
+          }
+        }
+        `
+    };
+
+    const token = this.context.token;
+
+    fetch(`http://localhost:8000/graphql`, {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Faild!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        this.fatchEvents();
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
+
+  fatchEvents() {
+    const requestBody = {
+      query: `
+        query {
+          events{
+            _id
+            title
+            description
+            price
+            date
+            creator{
+              _id
+              email
+            }
+          }
+        }
+        `
+    };
+
+    fetch(`http://localhost:8000/graphql`, {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Faild!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        const events = resData.data.events;
+        this.setState({ events: events });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   modalCancelHandler = () => {
     this.setState({ creating: false });
   };
 
   render() {
+    const eventList = this.state.events.map(event => {
+      return (
+        <li key={event._id} className="event__list-item">
+          {event.title}
+        </li>
+      );
+    });
+
     return (
       <React.Fragment>
         {this.state.creating && <Backdrop />}
@@ -33,15 +154,43 @@ export default class Events extends Component {
             onCancel={this.modalCancelHandler}
             onConfirm={this.modalConfirmHandler}
           >
-            <p>Modal Content</p>
+            <form>
+              <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" ref={this.titleElRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="price">Price</label>
+                <input type="number" id="price" ref={this.priceElRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="date">Date</label>
+                <input
+                  type="datetime-local"
+                  id="date"
+                  ref={this.dateElRef}
+                ></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  id="description"
+                  rows="4"
+                  ref={this.descriptionElRef}
+                ></textarea>
+              </div>
+            </form>
           </Modal>
         )}
-        <div className="events-control">
-          <p>Create your own Events!</p>
-          <button className="btn" onClick={this.startCreatEventHandler}>
-            Create Event
-          </button>
-        </div>
+        {this.context.token && (
+          <div className="events-control">
+            <p>Create your own Events!</p>
+            <button className="btn" onClick={this.startCreatEventHandler}>
+              Create Event
+            </button>
+          </div>
+        )}
+        <ul className="event__list">{eventList}</ul>
       </React.Fragment>
     );
   }
